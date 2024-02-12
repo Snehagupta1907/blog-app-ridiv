fc490b5e086c22acf795fbfbd08d43927f6dad79
Snehagupta1907 <snehagupta98930@gmail.com>
2024-02-12 21:56:24 +0530

    updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="py-4 bg-purple-700 text-white">
       <div className="container mx-auto">
         <div className="footer-content text-center">
-          <p className="font-rubik font-medium tracking-wide">© 2022 CopyRight Example. All rights reserved.</p>
+          <p className="font-rubik font-medium tracking-wide">© 2024 CopyRight Sneha Gupta. All rights reserved.</p>
         </div>
       </div>
     </footer>
